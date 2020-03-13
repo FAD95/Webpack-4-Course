160c16a17a110cc896973c365447cd4b952cd4be
Fadith Escorcia <feescorciam@protonmail.com>
2020-03-13 02:13:39 -0500

    images, videos and fonts support

--- url-loader/src/js/components/app.js
+++ url-loader/src/js/components/app.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import data from "../../data.json";
-import Loaders from "./loaders"
-console.log(data.loaders);
+import Loaders from "./loaders";
+import logo from "../../images/platzi.png";
+import video1 from "../../videos/que-es-core.mp4";
+import video2 from "../../videos/que-es-core.webm";
 
 function App() {
-
   const [loaderList, setloaderList] = useState([]);
-  const handleClick = ()=>{
-      setloaderList(data.loaders)
-  }
+  const handleClick = () => {
+    setloaderList(data.loaders);
+  };
   return (
     <div>
+      <header>
+        <img src={logo} alt="Logo Platzi" width={60} poster={logo}/>
+        <p>Que linda es mi app en react</p>
+      </header>
+      <main>
+      <video src={video1} controls width={420}></video>
       <ul>
-        {loaderList.map(item => <Loaders {...item} key={item.id}/>)}
+        {loaderList.map(item => (
+          <Loaders {...item} key={item.id} />
+        ))}
       </ul>
       <button onClick={handleClick}>List</button>
+      </main>
+      
     </div>
   );
 }
